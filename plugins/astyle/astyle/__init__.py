7e0fca53a2c2e18623216453f7ef67684a2b7e13
PyroSamurai <pyrosamurai@member.fsf.org>
2018-09-29 13:47:07 -0400

    Move style select from list to dropdown (WIP)
    Not a release.
    Changed some variable names and formatting.
    Beginning plans to modify style selection.
    Amend copyright info.

--- plugins/astyle/astyle/__init__.py
+++ plugins/astyle/astyle/__init__.py
@@ -13,60 +13,67 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 import gi
 from gi.repository import Gtk, GObject, Gedit, Gio, PeasGtk
-import subprocess, os
+import subprocess
+import os
 import os.path
 from os.path import expanduser
+gi.require_version('Gtk', '3.0')
+gi.require_version('PeasGtk', '1.0')
 
-HOME=expanduser("~")
+HOME = expanduser("~")
 FILENAME = HOME+"/.config/gedit/astyle"
 TREEVIEW = None
 ACCELERATOR = ['<Shift>F8']
-LANGUAGE = "allman"
-STYLES = [ "1tbs", "banner", "allman", "gnu", "google", "horstmann", "java",
-           "kr", "linux", "lisp", "pico", "stroustrup", "whitesmith" ]
+STYLE = "allman"
+STYLES = ["1tbs", "banner", "allman", "gnu", "google", "horstmann", "java",
+          "kr", "linux", "lisp", "pico", "stroustrup", "whitesmith"]
 
-################## FUNCTIONS ##################
-def get_index(listval, strval):
-    for itemval in listval:
-        if itemval == strval:
-            return listval.index(strval)
+
+# ################# FUNCTIONS ##################
+def get_index(strArray, strVal):
+    for index in strArray:
+        if index == strval:
+            return strArray.index(strVal)
     return 0
 
+
 def _save_setting(TALK):
-    global LANGUAGE
+    global STYLE
     global FILENAME
     w_file = open(FILENAME, "w")
     w_file.write(TALK)
     w_file.close()
 
+
 def _load_setting():
     global FILENAME
     if os.path.isfile(FILENAME):
         r_file = open(FILENAME, "r")
-        LLANGUAGE=r_file.read()
+        LSTYLE = r_file.read()
         r_file.close()
     else:
-        LLANGUAGE = "allman"
-        _save_setting(LLANGUAGE)
-    if not LLANGUAGE:
-        LLANGUAGE = "google"
-    return LLANGUAGE
+        LSTYLE = "allman"
+        _save_setting(LSTYLE)
+    if not LSTYLE:
+        LSTYLE = "allman"
+    return LSTYLE
+
 
 def _selection_changed(second):
-    global LANGUAGE
+    global STYLE
     global TREEVIEW
     (model, iter) = second.get_selected()
-    LANGUAGE = model[iter][0]
-    indexval = get_index(STYLES, LANGUAGE)
-    TREEVIEW.set_cursor(indexval);
-    _save_setting(LANGUAGE)
+    STYLE = model[iter][0]
+    index = get_index(STYLES, STYLE)
+    TREEVIEW.set_cursor(Gtk.TreePath, index, True)
+    _save_setting(STYLE)
     return True
 
+
 def create_configure_dialog():
-    global LANGUAGE
+    global STYLE
     global TREEVIEW
 
     vbox = Gtk.VBox()
@@ -78,28 +85,29 @@ def create_configure_dialog():
 
     TREEVIEW = Gtk.TreeView(liststore)
     cell = Gtk.CellRendererText()
-    column = Gtk.TreeViewColumn('Language Styles')
+    column = Gtk.TreeViewColumn('Formatting Styles')
     column.pack_start(cell, True)
     column.set_attributes(cell, text=0)
     column.set_sort_column_id(0)
     TREEVIEW.append_column(column)
-    indexval = get_index(STYLES, LANGUAGE)
-    TREEVIEW.set_cursor(indexval);
+    index = get_index(STYLES, STYLE)
+    TREEVIEW.set_cursor(, index,)
     tree_selection = TREEVIEW.get_selection()
     tree_selection.connect("changed", _selection_changed)
 
     vbox.pack_start(TREEVIEW, False, False, 0)
     return vbox
 
-################## MENU ##################
-class AStylePluginAppActivatable(GObject.Object, Gedit.AppActivatable, PeasGtk.Configurable):
-    __gtype_name__ = "AStylePlugin"
-    app = GObject.Property(type=Gedit.App)
+
+# ################# MENU ##################
+class App(GObject.Object, Gedit.AppActivatable, PeasGtk.Configurable):
+    __gtype_name__ = "AStyle"
+    app = GObject.property(type=Gedit.App)
 
     def __init__(self):
-        global LANGUAGE
+        global STYLE
         GObject.Object.__init__(self)
-        LANGUAGE = _load_setting()
+        STYLE = _load_setting()
 
     def do_activate(self):
         self.app.set_accels_for_action("win.astyle", ACCELERATOR)
@@ -115,15 +123,15 @@ class AStylePluginAppActivatable(GObject.Object, Gedit.AppActivatable, PeasGtk.C
         pass
 
 
-################## ACTION ##################
-class AStylePluginWindowActivatable(GObject.Object, Gedit.WindowActivatable, PeasGtk.Configurable):
+# ################# ACTION ##################
+class AppWindow(GObject.Object, Gedit.WindowActivatable, PeasGtk.Configurable):
     window = GObject.property(type=Gedit.Window)
 
     def __init__(self):
-        global LANGUAGE
+        global STYLE
         GObject.Object.__init__(self)
         self.settings = Gio.Settings.new("org.gnome.gedit.preferences.editor")
-        LANGUAGE = _load_setting()
+        STYLE = _load_setting()
 
     def do_create_configure_widget(self):
         print(" ")
@@ -136,32 +144,29 @@ class AStylePluginWindowActivatable(GObject.Object, Gedit.WindowActivatable, Pea
         action.connect('activate', self.format_code)
         self.window.add_action(action)
 
-    def format_code(self, action, parameter, user_data=None):
-        global LANGUAGE
-        document = self.window.get_active_document()
-        if not document:
-            return
-        start, end = document.get_bounds()
-        code = document.get_text(start, end, True)
-        result = self.go(code)
-        document.set_text(str(result, 'utf-8'))
-
-    def go (self, code):
-        global LANGUAGE
-
-        if LANGUAGE == None:
+    def run(self, code):
+        global STYLE
+        if STYLE is None:
             return code
-
         view = self.window.get_active_view()
-        tabwidth = view.get_tab_width()
-        tabval = str(tabwidth)
-        maxwidth=view.get_right_margin_position()
-        maxwidthval=str(maxwidth)
-        command = ["astyle --style="+LANGUAGE+" -xC"+ maxwidthval +" -c -s"+tabval]
+        tabLen = str(view.get_tab_width())
+        maxLen = str(view.get_right_margin_position())
+        options = "-xC"+maxLen+" -U -c -s"+tabLen
+        cmd = ["astyle --style="+STYLE+" "+options]
 
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        process.stdin.write(bytes(code, 'utf-8'))
-        process.stdin.close()
-        return process.stdout.read()
+        proc = subprocess.Popen(cmd, shell=True,
+                                stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        proc.stdin.write(bytes(code, 'utf-8'))
+        proc.stdin.close()
+        return proc.stdout.read()
 
+    def format_code(self, action, parameter, user_data=None):
+        global STYLE
+        doc = self.window.get_active_document()
+        if not doc:
+            return
+        start, end = doc.get_bounds()
+        code = doc.get_text(start, end, True)
+        result = self.run(code)
+        doc.set_text(str(result, 'utf-8'))
 
